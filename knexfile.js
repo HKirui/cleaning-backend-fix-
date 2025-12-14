24ff6eee7f9fd131c3c860b7bef68ef326aee3ff
HKirui <kiruiharold@gmail.com>
2025-12-14 11:18:41 +0300

    add ssl for railway and knex update

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'platform_user',
-      password: process.env.DB_PASSWORD || 'joymella',
-      database: process.env.DB_NAME || 'cleaning_platform',
-      port: process.env.DB_PORT || 5432,
+      host: "127.0.0.1",
+      user: "platform_user",
+      password: "joymella",
+      database: "cleaning_platform",
+      port: 5432,
     },
     migrations: {
       directory: './db/migrations',
